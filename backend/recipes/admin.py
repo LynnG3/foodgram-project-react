3f2d36cd47c6deb8cdcb90fa62cd712e214a5996
Полина Грунина <linaborisovsky@yandex.ru>
2024-01-23 15:09:11 +0300

    Модели, api views, urls, serializers, permissions ВЕБИНАР

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from recipes.models import Ingredient, Favorite, Recipe, RecipeIngredient
+from recipes.models import Ingredient, Favorite, Recipe, RecipeIngredient, Tag
 
 admin.site.register(Recipe)
 admin.site.register(Ingredient)
 admin.site.register(RecipeIngredient)
 admin.site.register(Favorite)
+admin.site.register(Tag)
