08226b93485a03ebded6b9e15000d5fd868e7048
Полина Грунина <linaborisovsky@yandex.ru>
2024-02-13 21:36:48 +0300

    автоматический айсорт, загвоздка с recipes_limit, поменяла нгинкс.конф и файл с менеджмент командой

--- backend/recipes/management/commands/import_csv.py
+++ backend/recipes/management/commands/import_csv.py
@@ -1,37 +1,46 @@
-import csv
+# import csv
 
-from django.conf import settings
+# from django.conf import settings
 from django.core.management.base import BaseCommand
 
-from recipes.models import Ingredient, Tag
+from recipes.models import Ingredient
 
-DICT_MODELS_RECIPES = {
-    Tag: settings.STATICFILES_DIRS / 'data/ingredients.csv',
-    Ingredient: settings.STATICFILES_DIRS / 'data/tags.csv',
-}
+# DICT_MODELS_RECIPES = {
+#     Tag: settings.STATICFILES_DIRS / 'data/ingredients.csv',
+#     Ingredient: settings.STATICFILES_DIRS / 'data/tags.csv',
+# }
 
+
+# class Command(BaseCommand):
+#     help = 'Заполняет базу данных данными из CSV-файлов'
+
+#     def handle(self, *args, **options):
+#         for key, value in DICT_MODELS_RECIPES.items():
+#             csv_file = value
+
+#             try:
+#                 model = key
+#             except NameError:
+#                 self.stdout.write(self.style.ERROR('Модель не найдена'))
+#                 return
+
+#             with open(csv_file, 'r', encoding='utf-8') as file:
+#                 reader = csv.DictReader(file)
+#                 model.objects.bulk_create(model(**row) for row in reader)
+
+#             self.stdout.write(self.style.SUCCESS(
+#                 f'Данные из {csv_file} успешно загружены в базу данных'
+#             ))
+
+#         self.stdout.write(self.style.SUCCESS(
+#             'Все данные успешно загружены в базу данных!'
+#         ))
 
 class Command(BaseCommand):
-    help = 'Заполняет базу данных данными из CSV-файлов'
-
-    def handle(self, *args, **options):
-        for key, value in DICT_MODELS_RECIPES.items():
-            csv_file = value
-
-            try:
-                model = key
-            except NameError:
-                self.stdout.write(self.style.ERROR('Модель не найдена'))
-                return
-
-            with open(csv_file, 'r', encoding='utf-8') as file:
-                reader = csv.DictReader(file)
-                model.objects.bulk_create(model(**row) for row in reader)
-
-            self.stdout.write(self.style.SUCCESS(
-                f'Данные из {csv_file} успешно загружены в базу данных'
-            ))
-
-        self.stdout.write(self.style.SUCCESS(
-            'Все данные успешно загружены в базу данных!'
-        ))
+    def handle(self, **options):
+        with open("ingredients.csv") as f:
+            for line in f:
+                ing = line.split(',')
+                Ingredient.objects.get_or_create(
+                    name=ing[0], measurement_unit=ing[1]
+                )
