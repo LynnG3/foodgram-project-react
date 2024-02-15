fc2d9cf305f77c437f306ee849d78ee6b8df5c32
Полина Грунина <linaborisovsky@yandex.ru>
2024-02-15 20:13:15 +0300

    исправлен media root обратно

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -125,8 +125,8 @@ REST_FRAMEWORK = {
 
 MEDIA_URL = '/media/'
 
-# MEDIA_ROOT = '/app/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/app/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
 # STATIC_URL = '/static/django/'
