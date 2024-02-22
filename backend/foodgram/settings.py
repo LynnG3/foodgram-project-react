c8d46707a1831b010f6f382444a70696a1ba6811
Полина Грунина <linaborisovsky@yandex.ru>
2024-02-23 00:25:34 +0300

    Review1.8 collected_static

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -121,7 +121,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / 'collected_static'
+# STATIC_ROOT = BASE_DIR / 'collected_static'
+STATIC_ROOT = '/app/collected_static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'data'),)
 
